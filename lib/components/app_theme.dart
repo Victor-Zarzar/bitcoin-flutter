2d5167e1e2cf2379826a411b2d03c6dafff3996d
victorzarzar <victorzarzar58@gmail.com>
2024-02-17 19:49:29 -0300

    first commit

---
+++ lib/components/app_theme.dart
@@ -0,0 +1,59 @@
+import 'dart:ui';
+
+class AppTheme {
+  AppTheme._();
+
+  static Color get primaryColor => const Color(0xfff4f6f8);
+
+  static Color get secondaryColor => const Color(0xFF546E7A);
+}
+
+class BackGround {
+  BackGround._();
+
+  static Color get primaryColor => const Color(0xFFECEFF1);
+}
+
+class TabBarColor {
+  TabBarColor._();
+
+  static Color get primaryColor => const Color(0xFF455A64);
+
+  static Color get secondaryColor => const Color(0xFF78909C);
+
+  static Color get thirdColor => const Color(0xffF35383);
+}
+
+class PostColor {
+  PostColor._();
+
+  static Color get primaryColor => const Color(0xFF78909C);
+
+  static Color get secondaryColor => const Color(0xFF000000);
+}
+
+class CardColor {
+  CardColor._();
+
+  static Color get primaryColor => const Color(0xFFBDBDBD);
+
+  static Color get secondaryColor => const Color(0xFF9E9E9E);
+
+  static Color get thirdColor => const Color(0xFFFFFFFF);
+
+  static Color get fourthColor => const Color(0xFF000000);
+}
+
+class FontTextColor {
+  FontTextColor._();
+
+  static Color get primaryColor => const Color(0xFFFFFFFF);
+
+  static Color get secondaryColor => const Color(0xFF263238);
+}
+
+class TextButtonColor {
+  TextButtonColor._();
+
+  static Color get primaryColor => const Color.fromARGB(255, 182, 6, 59);
+}
---
+++ lib/components/assets_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ImageAsset {
+  ImageAsset._();
+
+  static Image asset() {
+    return Image.asset('lib/assets/.jpg');
+  }
+}
+
