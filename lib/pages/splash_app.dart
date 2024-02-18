d98868f685395cfc3b484f9e78b6aa87899bac11
victorzarzar <victorzarzar58@gmail.com>
2024-02-18 19:21:48 -0300

    updates in splash app

--- lib/pages/splash_app.dart
+++ lib/pages/splash_app.dart
@@ -1,6 +1,7 @@
 import 'package:bitcoin_flutter/components/app_theme.dart';
 import 'package:bitcoin_flutter/components/assets_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -21,37 +22,53 @@ class _SplashState extends State<Splash> {
           height: myHeight,
           width: myWidth,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               ImageOne.asset(),
               const Text(
                 'The Future',
                 style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-              Text(
-                'Learn more about cryptocurrency, look to',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: FontTextColor.primaryColor),
+              Column(
+                children: [
+                  Text(
+                    'Learn more about cryptocurrency, look to',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: FontTextColor.primaryColor),
+                  ),
+                  Text(
+                    'the future in IO Crypto',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.normal,
+                        color: FontTextColor.primaryColor),
+                  ),
+                ],
               ),
-              Text(
-                'the future in IO Crypto',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.normal,
-                    color: FontTextColor.primaryColor),
-              ),
-              Container(
-                decoration: BoxDecoration(color: SplashColor.primaryColor),
-                child: const Row(
-                  children: [
-                    Text(
-                      'Create',
-                      style: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.normal),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: myWidth * 0.14),
+                child: Container(
+                  decoration: BoxDecoration(color: SplashColor.primaryColor),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: myWidth * 0.05, vertical: myHeight * 0.013),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Create',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.normal),
+                        ),
+                        RotationTransition(
+                          turns: AlwaysStoppedAnimation(310 / 360),
+                          child: Icon(Icons.arrow_forward_rounded),
+                        ),
+                      ],
                     ),
-                    Icon(Icons.arrow_forward_rounded),
-                  ],
+                  ),
                 ),
               ),
             ],
